904c7f8d3612756f1c8676c8c0c1ac01fb83dcbc
Jaffar Abbas <gamaportal8@gmail.com>
2022-03-11 17:28:26 +0500

    11/03/2022 17:27:59

--- Dotnet Unit Test Framework/AutomationUsingSelenium_1/AutomationUsingSelenium_1/UnitTest1.cs
+++ Dotnet Unit Test Framework/AutomationUsingSelenium_1/AutomationUsingSelenium_1/UnitTest1.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 
 namespace AutomationUsingSelenium_1
@@ -6,8 +8,30 @@ namespace AutomationUsingSelenium_1
     [TestClass]
     public class UnitTest1
     {
+        IWebDriver driver;
+        public UnitTest1()
+        {
+            driver = new ChromeDriver();
+        }
+        public void LoadTest()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://adactinhotelapp.com/";
+        }
+
         [TestMethod]
-        public void TestMethod1()
+        [TestCategory("Login")]
+        public void TestMethod001()
+        {
+            LoadTest();
+            driver.FindElement(By.Id("username")).SendKeys("JaffarAbbas");
+            driver.FindElement(By.Name("password")).SendKeys("JaffarAbbas");
+            driver.FindElement(By.ClassName("login_button")).Click();
+            //driver.Close();
+        }
+
+        [TestMethod]
+        public void TestMethod002()
         {
         }
     }
