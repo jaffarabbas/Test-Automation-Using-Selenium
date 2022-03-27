6143c7339f551ca35b6282bacd637aa0a90b570e
Jaffar Abbas <gamaportal8@gmail.com>
2022-03-27 13:13:03 +0500

    27/03/2022 13:13:01

---
+++ Dotnet Unit Test Framework/SeleniumBasics/SeleniumBasics/TestInitialize/LoadDriver.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumBasics.TestInitialize
+{
+    public class LoadDriver
+    {
+        private static LoadDriver driver = new LoadDriver();
+        public IWebDriver webDriver;
+        public IJavaScriptExecutor js;
+        public LoadDriver(){}
+        /// <summary>
+        /// Single ton pattern for initialize only one object of load driver class
+        /// </summary>
+        public static LoadDriver InitializeDriver()
+        {
+            return driver;
+        }
+        /// <summary>
+        /// Run Url for driver and exicute web drivers
+        /// </summary>ss
+        public void RunDriver(String url)
+        {
+            webDriver = new ChromeDriver();
+            js = webDriver as IJavaScriptExecutor;
+            webDriver.Url = url;
+        }
+    }
+}
