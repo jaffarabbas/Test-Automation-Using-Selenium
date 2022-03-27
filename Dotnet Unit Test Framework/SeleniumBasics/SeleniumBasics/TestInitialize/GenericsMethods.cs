dcef7494b725fa755178dc0452e459c94d98da86
Jaffar Abbas <gamaportal8@gmail.com>
2022-03-27 13:13:03 +0500

    27/03/2022 13:13:01

---
+++ Dotnet Unit Test Framework/SeleniumBasics/SeleniumBasics/TestInitialize/GenericsMethods.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumBasics.TestInitialize
+{
+    public class GenericsMethods
+    {
+        public void SendValue(By id, string value,IWebDriver wbDriver)
+        {
+            wbDriver.FindElement(id).SendKeys(value);
+        }
+    }
+}
