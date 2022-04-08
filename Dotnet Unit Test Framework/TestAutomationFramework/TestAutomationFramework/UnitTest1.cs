38691c2d7e131909af6a47db5d75a3791b790da1
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-08 20:43:48 +0500

    08/04/2022 20:43:42

---
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestAutomationFramework
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
