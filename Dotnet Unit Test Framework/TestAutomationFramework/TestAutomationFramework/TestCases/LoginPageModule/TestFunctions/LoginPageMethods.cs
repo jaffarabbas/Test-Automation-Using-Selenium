de2f171d4dfc62c000216c6bf24e06ce8d0e5f83
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-09 20:15:16 +0500

    09/04/2022 20:15:14

---
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/LoginPageModule/TestFunctions/LoginPageMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoadDriver;
+
+namespace TestAutomationFramework.TestCases.LoginPage.TestFunctions
+{
+    public class LoginPageMethods : LoadDriverInitialiazer
+    {
+        public void Login(string url,string username,string password)
+        {
+            driver.Url = url;
+        }
+    }
+}
