bcb082b3dd63cac4b39a1244bae043fc5d419fcb
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-09 20:15:16 +0500

    09/04/2022 20:15:14

---
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/LoadDriver/LoadDriver.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadDriver
+{
+    public class LoadDriverInitialiazer
+    {
+        public static IWebDriver driver;
+        public static IJavaScriptExecutor jsDriver;
+        public static void LoadWebDriver(string driverType)
+        {
+            try
+            {
+                if (driverType == "Chrome")
+                {
+                    var chromeOptions = new ChromeOptions();
+                    chromeOptions.AddArguments("--start-maximized");
+                    IWebDriver chromeDriver = new ChromeDriver(chromeOptions);
+                    jsDriver = chromeDriver as IJavaScriptExecutor;
+                    driver = chromeDriver;
+                }
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
