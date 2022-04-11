7d8cadeae906d72fa0cea5e7daa5c61a9c8f70b0
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-11 13:53:33 +0500

    11/04/2022 13:53:11

---
+++ Test Driven development/PasswordValidator/PasswordValidator/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordValidator
+{
+    public class PassowrdValidatorClass
+    {
+        public string username;
+        public string password;
+        public int Validator()
+        {
+            if(password.Length == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                if(password.Length < 8)
+                {
+                    return -1;
+                }
+                else if (!password.Any(char.IsUpper))
+                {
+                    return -2;
+                }
+                else if (!password.Any(char.IsLower))
+                {
+                    return -3;
+                }
+                else if (!password.Any(char.IsDigit))
+                {
+                    return -4;
+                }
+            }
+            return 1;
+        }
+    }
+}
