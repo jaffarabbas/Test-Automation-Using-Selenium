f6f22abb6fc48c8587b4498d2c3abe199fa832af
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-11 13:53:39 +0500

    11/04/2022 13:53:11

---
+++ Test Driven development/TDD_Class_5/TDD_Class_5/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Class_5
+{
+    public class DistanceClass
+    {
+        public double Ditance(double speed,double time)
+        {
+            return speed * time;
+        }
+
+        public double CordinateDistance(double[] x,double[] y) 
+        {
+            return Math.Sqrt((Math.Pow(x[0] - x[1],2)) + (Math.Pow(y[0] - y[1],2)));
+        }
+    }
+}
