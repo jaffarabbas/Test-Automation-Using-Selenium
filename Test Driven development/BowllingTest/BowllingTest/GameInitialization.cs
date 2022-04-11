f7a244944636b0fae3b2a93996272df0e48ad24d
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-11 13:53:20 +0500

    11/04/2022 13:53:11

---
+++ Test Driven development/BowllingTest/BowllingTest/GameInitialization.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BowlingGame;
+namespace BowllingTest
+{
+    public class GameInitialization
+    {
+        public Game game;
+        public int frame;
+        public GameInitialization()
+        {
+            game = new Game();
+            frame = 10;
+        }
+        public void TearDown()
+        {
+            game = null;
+            frame = 0;
+        }
+        ~GameInitialization()
+        {
+            game = null;
+            frame = 0;
+        }
+        public void RollStrike()
+        {
+            game.Roll(10);
+        }
+
+        public void FrameHits(int pin, int count)
+        {
+            for(int i=0; i< count; i++)
+            {
+                game.Roll(pin);
+            }
+        }
+
+        public void RollSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
+        }
+    }
+}
