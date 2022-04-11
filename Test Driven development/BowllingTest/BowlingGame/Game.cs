2ed60f64d90dcd9e1712ef1be2245590aec28bff
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-11 13:53:19 +0500

    11/04/2022 13:53:11

---
+++ Test Driven development/BowllingTest/BowlingGame/Game.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private readonly int[] _rolls = new int[21];
+        private int _currentRoll;
+        /// <summary>
+        /// Rolls the specified pins.
+        /// </summary>
+        /// <param name="pins">The pins.</param>
+        public void Roll(int pins) => _rolls[_currentRoll++] = pins;
+        /// <summary>
+        /// Scores this instance.
+        /// </summary>
+        /// <returns></returns>
+        public int Score()
+        {
+            var score = 0;
+            var frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(frameIndex))
+                {
+                    score += StrikeBonus(frameIndex);
+                    frameIndex++;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    score += 10 + SpareBonus(frameIndex);
+                    frameIndex += 2;
+                }
+
+                else
+                {
+                    score += SumOfBallsInFrames(frameIndex);
+                    frameIndex += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private bool IsStrike(int frameIndex) => _rolls[frameIndex] == 10;
+
+        private int SumOfBallsInFrames(int frameIndex) => _rolls[frameIndex] + _rolls[frameIndex + 1];
+
+        private int SpareBonus(int frameIndex) => _rolls[frameIndex + 2];
+
+        private bool IsSpare(int frameIndex) => _rolls[frameIndex] + _rolls[frameIndex + 1] == 10;
+
+        private int StrikeBonus(int frameIndex) => 10 + _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+    }
+}
