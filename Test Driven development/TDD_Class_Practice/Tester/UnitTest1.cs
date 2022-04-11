57f8d084b7b9ebb25233f230615cbedf8c63001b
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-11 13:53:44 +0500

    11/04/2022 13:53:11

---
+++ Test Driven development/TDD_Class_Practice/Tester/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TDD_Class_Practice;
+
+namespace Tester
+{
+    [TestClass]
+    [TestCategory("Calculator")]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AdditionWithWorngInput()
+        {
+            Class1 o = new Class1();
+            int value = o.add(4, 5);
+            int expectedValue = 5;
+            Assert.AreEqual(expectedValue, value);
+        }
+
+
+        [TestMethod]
+        public void AdditionWithRightInput()
+        {
+            Class1 o = new Class1();
+            int value = o.add(4, 5);
+            int expectedValue = 9;
+            Assert.AreEqual(expectedValue, value);
+        }
+    }
+}
