0fbbe7684a3fe0d8d7ed6db4a5a19d095cf73e93
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-11 13:53:21 +0500

    11/04/2022 13:53:11

---
+++ Test Driven development/BowllingTest/BowllingTest/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BowllingTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        GameInitialization initalizeGame;
+        public UnitTest1()
+        {
+            initalizeGame = new GameInitialization();
+        }
+        [TestMethod]
+        public void BLFullGameScore()
+        {
+            initalizeGame.FrameHits(10, 12);
+            Assert.AreEqual(300, initalizeGame.game.Score());
+        }
+
+        [TestMethod]
+        public void BLScores()
+        {
+            initalizeGame.FrameHits(0, initalizeGame.frame);
+            Assert.AreEqual(0, initalizeGame.game.Score());
+        }
+
+        [TestMethod]
+        public void BLSingleScores()
+        {
+            initalizeGame.game.Roll(0);
+            Assert.AreEqual(0, initalizeGame.game.Score());
+        }
+
+        [TestMethod]
+        public void BLSpareScores()
+        {
+            initalizeGame.RollSpare();
+            initalizeGame.game.Roll(3);
+            initalizeGame.FrameHits(0, 17);
+            Assert.AreEqual(16, initalizeGame.game.Score());
+        }
+
+        [TestMethod]
+        public void BLStrikeScore()
+        {
+            initalizeGame.RollStrike();
+            initalizeGame.game.Roll(3);
+            initalizeGame.game.Roll(4);
+            initalizeGame.FrameHits(0, 16);
+            Console.WriteLine("Roll Total - {0}, Result - {1}",24,initalizeGame.game.Score());
+            Assert.AreEqual(24, initalizeGame.game.Score());
+        }
+    }
+}
