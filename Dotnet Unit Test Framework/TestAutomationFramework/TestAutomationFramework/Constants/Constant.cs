a77232dc95a5b13a897d00a4b55406108b7e9286
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-29 22:09:12 +0500

    Inserted TestAutomationFramework/TestAutomationFramework/Constants/Constant.cs at 29/04/2022 22:09:12

---
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/Constants/Constant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomationFramework.Constants
+{
+    public class ApplicationConstant
+    {
+        /// <summary>
+        /// xml connection string for data driven
+        /// </summary>
+        public const string xmlConnectionString = "Microsoft.VisualStudio.TestTools.DataSource.XML";
+        /// <summary>
+        /// xml path for data driven
+        /// </summary>
+        public const string xmlPath = @"DataClass\data.xml";
+    }
+}
