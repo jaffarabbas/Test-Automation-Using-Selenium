fc9f8bfc5554b2eb6a86703246cbb39a8b399358
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-29 22:09:13 +0500

    Inserted TestAutomationFramework/TestAutomationFramework/TestCases/LoginPageModule/UnitTest1.cs at 29/04/2022 22:09:12

---
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/LoginPageModule/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Windows.Forms;
+
+namespace TestAutomationFramework.TestCases.LoginPageModule
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        IWebDriver driver;
+        public UnitTest1()
+        {
+            driver = new ChromeDriver();
+        }
+        public void LoadTest()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://adactinhotelapp.com/";
+        }
+      
+        [TestMethod]
+        //[DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", @"data.xml", "Login", DataAccessMethod.Sequential)]
+        [Description("Test Login with Data driven from xml file data.xml")]
+        [TestCategory("XML Driven Login")]
+        public void TestMethod1()
+        {
+            string username = "";
+            string password = "";
+            string message = "";
+            string locator = "";
+            LoadTest();
+            driver.FindElement(By.Id("username")).SendKeys(username);
+            driver.FindElement(By.Name("password")).SendKeys(password);
+            driver.FindElement(By.ClassName("login_button")).Click();
+            string messageValue = driver.FindElement(By.ClassName(locator)).Text;
+            Assert.AreEqual(message, messageValue, "Assert Failed");
+            driver.Close();
+        }
+    }
+}
