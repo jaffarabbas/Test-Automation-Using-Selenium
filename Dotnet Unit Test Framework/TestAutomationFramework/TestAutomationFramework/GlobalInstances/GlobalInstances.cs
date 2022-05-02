75b7e316f3c7a0b1a973ee5bb2c2177d40e9b9e9
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-02 20:48:14 +0500

    Inserted TestAutomationFramework/TestAutomationFramework/GlobalInstances/GlobalInstances.cs at 02/05/2022 20:48:13

---
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/GlobalInstances/GlobalInstances.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomationFramework.GlobalItems
+{
+    static class GlobalInstances
+    {
+        public static Dictionary<string, string> instancesDictionary = new Dictionary<string, string>();
+    }
+}
