c635330bed3e41584061be82a4d2fb61ff31e309
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-02 20:48:14 +0500

    Inserted TestAutomationFramework/TestAutomationFramework/TestCases/AssignLeavePageModule/AssignTestCases.cs at 02/05/2022 20:48:14

---
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/AssignLeavePageModule/AssignTestCases.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestAutomationFramework.TestCases.AssignLeavePageModule.TestFunctions;
+
+namespace TestAutomationFramework.TestCases.AssignLeavePageModule
+{
+    [TestClass]
+    public class AssignTestCases
+    {
+        [TestMethod]
+        [TestCategory("Assign Leave")]
+        //[DataSource(ApplicationConstant.xmlConnectionString, ApplicationConstant.xmlPath, "Login", DataAccessMethod.Sequential)]
+        [Description("successfull Assign Leave test with postive test data")]
+        public void TestAssignLeaveForm002()
+        {
+            AssignLeaveMethods.SubmitForm();
+        }
+    }
+}
