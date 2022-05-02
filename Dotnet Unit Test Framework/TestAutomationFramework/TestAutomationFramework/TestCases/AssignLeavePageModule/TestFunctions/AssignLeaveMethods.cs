863017399fd1bd227dcee8228417c11125daa5b6
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-02 20:48:15 +0500

    Inserted TestAutomationFramework/TestAutomationFramework/TestCases/AssignLeavePageModule/TestFunctions/AssignLeaveMethods.cs at 02/05/2022 20:48:14

---
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/AssignLeavePageModule/TestFunctions/AssignLeaveMethods.cs
@@ -0,0 +1,18 @@
+﻿using LoadDriver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestAutomationFramework.TestCases.AssignLeavePageModule.TestFunctions
+{
+    public class AssignLeaveMethods : LoadDriverInitialiazer
+    {
+        public static void SubmitForm()
+        {
+            MessageBox.Show("asd");
+        } 
+    }
+}
