ce89fb26d204a531dd548e42ed2ece18842299a2
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-04 23:45:59 +0500

    Updated TestAutomationFramework/TestAutomationFramework/TestCases/AssignLeavePageModule/AssignTestCases.cs at 04/05/2022 23:45:58

--- Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/AssignLeavePageModule/AssignTestCases.cs
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/AssignLeavePageModule/AssignTestCases.cs
@@ -1,19 +1,84 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Configuration;
+using TestAutomationFramework.Constants;
+using TestAutomationFramework.GlobalItems;
 using TestAutomationFramework.TestCases.AssignLeavePageModule.TestFunctions;
 
 namespace TestAutomationFramework.TestCases.AssignLeavePageModule
 {
     [TestClass]
-    public class AssignTestCases
+    public class AssignLeaveTestCases
     {
+        //#region Instance Properties
+
+        ///// <summary>
+        ///// Object instances
+        ///// </summary>
+        //public static AssignLeaveMethods assignLeavePageMethods;
+
+        //#endregion
+
+        //#region Text Context Inititalization
+
+        //public TestContext instance;
+        //public TestContext TestContext
+        //{
+        //    set { instance = value; }
+        //    get { return instance; }
+        //}
+
+        //#endregion
+
+        //#region Initializations and Cleanups
+
+        //[AssemblyInitialize]
+        //public static void AssemblyInit(TestContext context)
+        //{
+
+        //}
+
+        //[AssemblyCleanup]
+        //public static void AssemblyCleanup()
+        //{
+
+        //}
+
+        //[ClassInitialize]
+        //public static void ClassInit(TestContext context)
+        //{
+        //    GlobalInstances.SetInstancesDictionary("browserConnection", ConfigurationManager.AppSettings.Get("ChromeBrowser"));
+        //    GlobalInstances.SetInstancesDictionary("url", ApplicationConstant.mainUrl);
+        //    //assignLeavePageMethods = new AssignLeaveMethods();
+        //}
+
+        //[ClassCleanup]
+        //public static void ClassCleanup()
+        //{
+
+        //}
+
+        //[TestInitialize]
+        //public void TestInit()
+        //{
+
+        //}
+
+        //[TestCleanup]
+        //public void TestCleanup()
+        //{
+
+        //}
+
+        //#endregion
+
         [TestMethod]
         [TestCategory("Assign Leave")]
-        //[DataSource(ApplicationConstant.xmlConnectionString, ApplicationConstant.xmlPath, "Login", DataAccessMethod.Sequential)]
+        //[DataSource(ApplicationConstant.xmlConnectionString, ApplicationConstant.xmlPath, "Login", DataAccessMethod.Sequential)]A
         [Description("successfull Assign Leave test with postive test data")]
         public void TestAssignLeaveForm002()
         {
-            AssignLeaveMethods.SubmitForm();
+            //AssignLeaveMethods.SubmitForm();
         }
     }
 }
