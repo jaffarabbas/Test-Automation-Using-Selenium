2ebc0fabac56cb4c20ca0ef1c4c9984b53cf9658
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-04 23:45:55 +0500

    Inserted TestAutomationFramework/TestAutomationFramework/Validations/Validations.cs at 04/05/2022 23:45:53

---
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/Validations/Validations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomationFramework.Validations
+{
+    public class Validation
+    {
+        public static bool DictionaryValidation<T>(Dictionary<string, T> dictionary, string key, dynamic value)
+        {
+            return !dictionary.ContainsKey(key) && value != null && value != "";
+        }
+    }
+}
