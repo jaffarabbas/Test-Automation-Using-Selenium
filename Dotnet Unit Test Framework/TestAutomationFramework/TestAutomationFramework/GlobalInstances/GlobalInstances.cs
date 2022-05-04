ff8644c0a69b593aafe90600fbf16e8ae3b6d00f
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-04 23:45:57 +0500

    Updated TestAutomationFramework/TestAutomationFramework/GlobalInstances/GlobalInstances.cs at 04/05/2022 23:45:57

--- Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/GlobalInstances/GlobalInstances.cs
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/GlobalInstances/GlobalInstances.cs
@@ -1,13 +1,33 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestAutomationFramework.Validations;
 
 namespace TestAutomationFramework.GlobalItems
 {
     static class GlobalInstances
     {
-        public static Dictionary<string, string> instancesDictionary = new Dictionary<string, string>();
+        private static Dictionary<string, string> instancesDictionary = new Dictionary<string, string>(); 
+
+        public static void SetInstancesDictionary(string key,string value)
+        {
+            if (Validation.DictionaryValidation(instancesDictionary, key, value))
+            {
+                instancesDictionary.Add(key, value);
+            }
+        }
+
+        public static Dictionary<string,string> GetInstancesDictionary()
+        {
+            return instancesDictionary;
+        }
+
+        public static void ClearInstancesDictionary()
+        {
+            instancesDictionary.Clear();
+        }
     }
 }
