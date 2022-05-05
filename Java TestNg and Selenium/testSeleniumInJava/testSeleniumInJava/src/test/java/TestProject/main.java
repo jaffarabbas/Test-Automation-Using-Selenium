f88f15ffd891630c60b79e430581aeb561816b28
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-05 23:09:12 +0500

    Updated testSeleniumInJava/testSeleniumInJava/src/test/java/TestProject/main.java at 05/05/2022 23:09:12

--- Java TestNg and Selenium/testSeleniumInJava/testSeleniumInJava/src/test/java/TestProject/main.java
+++ Java TestNg and Selenium/testSeleniumInJava/testSeleniumInJava/src/test/java/TestProject/main.java
@@ -1,2 +1,33 @@
-package TestProject;public class main {
+package TestProject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class main {
+    public static void main(String[] args) throws InterruptedException {
+        String webDriverExtension= "webdriver.chrome.driver";
+        String webDriverPath = "J:\\softwares\\chromedriver_win32\\chromedriver.exe";
+        String url = "https://opensource-demo.orangehrmlive.com/index.php/auth/login";
+        System.setProperty(webDriverExtension,webDriverPath);
+
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get(url);
+        webDriver.manage().window().maximize();
+        webDriver.findElement(By.id("txtUsername")).sendKeys("admin");
+        webDriver.findElement(By.name("txtPassword")).sendKeys("admin123");
+        webDriver.findElement(By.id("btnLogin")).click();
+        String text = webDriver.findElement(By.xpath("//*[@id='content']/div/div[1]/h1")).getText();
+        if (text.contains("Dashboard"))
+            System.out.println("Login Successfully");
+        webDriver.findElement(By.xpath("//*[@id='welcome']")).click();
+        Thread.sleep(2000);
+        webDriver.findElement(By.linkText("Logout")).click();
+        webDriver.navigate().refresh();
+        String title = webDriver.getTitle();
+        System.out.println(title);
+        String currentUrl = webDriver.getCurrentUrl();
+        System.out.println(currentUrl);
+        webDriver.quit();
+    }
 }
