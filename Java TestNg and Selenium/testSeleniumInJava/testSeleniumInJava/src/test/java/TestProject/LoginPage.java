ce63a83dd364da7e2bef29a86ca1d2d083261bce
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-06 10:29:47 +0500

    Updated testSeleniumInJava/testSeleniumInJava/src/test/java/TestProject/LoginPage.java at 06/05/2022 10:29:46

--- Java TestNg and Selenium/testSeleniumInJava/testSeleniumInJava/src/test/java/TestProject/LoginPage.java
+++ Java TestNg and Selenium/testSeleniumInJava/testSeleniumInJava/src/test/java/TestProject/LoginPage.java
@@ -1,2 +1,21 @@
-package TestProject;public class LoginPage {
+package TestProject;
+
+import org.openqa.selenium.By;
+
+public class LoginPage extends BaseClass{
+    By userId = new By.ById("txtUsername");
+    By passId = new By.ByName("txtPassword");
+    By btnId = new By.ById("btnLogin");
+    By menu = new By.ByXPath("//*[@id='welcome']");
+    By logoutLink = new By.ByLinkText("Logout");
+    public void login(String username,String password){
+        webDriver.findElement(userId).sendKeys(username);
+        webDriver.findElement(passId).sendKeys(password);
+        webDriver.findElement(btnId).click();
+    }
+    public void logout() throws InterruptedException {
+        webDriver.findElement(menu).click();
+        Thread.sleep(2000);
+        webDriver.findElement(logoutLink).click();
+    }
 }
