17203885856f944f2698f4f4000eab7fd231ba09
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-07 19:25:07 +0500

    Updated testSeleniumInJava/testSeleniumInJava/src/test/java/TestProject/main.java at 07/05/2022 19:25:07

--- Java TestNg and Selenium/testSeleniumInJava/testSeleniumInJava/src/test/java/TestProject/main.java
+++ Java TestNg and Selenium/testSeleniumInJava/testSeleniumInJava/src/test/java/TestProject/main.java
@@ -3,11 +3,16 @@ package TestProject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import utils.ConfigUtils;
+
+import java.util.Properties;
 
 public class main {
     public static void main(String[] args) throws InterruptedException {
         LoginPage loginPage = new LoginPage();
-        loginPage.url = "https://opensource-demo.orangehrmlive.com/index.php/auth/login";
+        Properties properties = ConfigUtils.getProps("data");
+
+        loginPage.url = properties.getProperty("URL");
         loginPage.Initialize();
         loginPage.login("admin","admin123");
         String text = loginPage.webDriver.findElement(By.xpath("//*[@id='content']/div/div[1]/h1")).getText();
