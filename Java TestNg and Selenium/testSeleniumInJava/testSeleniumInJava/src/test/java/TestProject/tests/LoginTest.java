f92d05d203fe596a98a01f8dfb1f86b8a45b4ef1
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-08 11:25:34 +0500

    Updated testSeleniumInJava/testSeleniumInJava/src/test/java/TestProject/tests/LoginTest.java at 08/05/2022 11:25:33

--- Java TestNg and Selenium/testSeleniumInJava/testSeleniumInJava/src/test/java/TestProject/tests/LoginTest.java
+++ Java TestNg and Selenium/testSeleniumInJava/testSeleniumInJava/src/test/java/TestProject/tests/LoginTest.java
@@ -1,2 +1,42 @@
-package TestProject.tests;public class LoginTest {
+package TestProject.tests;
+
+import TestProject.LoginPage;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utils.ConfigUtils;
+
+import java.util.Properties;
+
+public class LoginTest {
+    LoginPage loginPage;
+    Properties properties;
+    @BeforeClass
+    public void Initialize(){
+        loginPage = new LoginPage();
+        properties = ConfigUtils.getProps("data");
+        loginPage.url = properties.getProperty("URL");
+    }
+
+    @Test(description = "Login test",priority = 1,suiteName = "Login")
+    public void login(){
+        loginPage.Initialize();
+        loginPage.login("admin","admin123");
+        String text = loginPage.webDriver.findElement(By.xpath("//*[@id='content']/div/div[1]/h1")).getText();
+        Assert.assertEquals(text,"Dashboard");
+    }
+
+    @Test(description = "Logout test",priority = 2,suiteName = "Login")
+    public void logout() throws InterruptedException {
+        loginPage.logout();
+        String currentUrl = loginPage.webDriver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("/login"));
+    }
+
+    @AfterClass
+    public void cleanUp(){
+        loginPage.webDriver.quit();
+    }
 }
