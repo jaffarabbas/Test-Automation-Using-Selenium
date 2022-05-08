70377ec814c27c6c82ee88b75154130363ac29e0
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-08 11:25:33 +0500

    Updated testSeleniumInJava/testSeleniumInJava/src/test/java/TestProject/main.java at 08/05/2022 11:25:33

--- Java TestNg and Selenium/testSeleniumInJava/testSeleniumInJava/src/test/java/TestProject/main.java
+++ Java TestNg and Selenium/testSeleniumInJava/testSeleniumInJava/src/test/java/TestProject/main.java
@@ -9,20 +9,6 @@ import java.util.Properties;
 
 public class main {
     public static void main(String[] args) throws InterruptedException {
-        LoginPage loginPage = new LoginPage();
-        Properties properties = ConfigUtils.getProps("data");
 
-        loginPage.url = properties.getProperty("URL");
-        loginPage.Initialize();
-        loginPage.login("admin","admin123");
-        String text = loginPage.webDriver.findElement(By.xpath("//*[@id='content']/div/div[1]/h1")).getText();
-        if (text.contains("Dashboard"))
-            System.out.println("Login Successfully");
-        loginPage.webDriver.navigate().refresh();
-        loginPage.logout();
-        String currentUrl = loginPage.webDriver.getCurrentUrl();
-        if (currentUrl.contains("/login"))
-            System.out.println("Logout successfully");
-        loginPage.webDriver.quit();
     }
 }
