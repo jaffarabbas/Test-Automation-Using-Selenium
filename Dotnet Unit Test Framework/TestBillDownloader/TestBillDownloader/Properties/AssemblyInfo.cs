7daee8ba00613027a832a29a34f1912ccd1abf2f
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-15 18:39:23 +0500

    Inserted TestBillDownloader/TestBillDownloader/Properties/AssemblyInfo.cs at 15/05/2022 18:39:23

---
+++ Dotnet Unit Test Framework/TestBillDownloader/TestBillDownloader/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestBillDownloader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestBillDownloader")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("065e97c9-dae5-4fcf-b960-5f432f7d0683")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
