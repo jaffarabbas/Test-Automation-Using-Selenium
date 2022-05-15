cf1f3d24607f6a60730430f7035a25a551ea8afa
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-15 18:39:22 +0500

    Inserted TestBillDownloader/TestBillDownloader/Constants/Paths.cs at 15/05/2022 18:39:22

---
+++ Dotnet Unit Test Framework/TestBillDownloader/TestBillDownloader/Constants/Paths.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBillDownloader.Constants
+{
+    public class Paths
+    {
+        public const string XmlConnectionString = "Microsoft.VisualStudio.TestTools.DataSource.XML";
+        public const string XmlPath = @"J:\Github\Test-Automation-Using-Selenium\Dotnet Unit Test Framework\TestBillDownloader\TestBillDownloader\DataSource\data.xml";
+        public const string DownloadPath = @"J:\Github\Test-Automation-Using-Selenium\Dotnet Unit Test Framework\TestBillDownloader\TestBillDownloader\Downloads\";
+    }
+}
