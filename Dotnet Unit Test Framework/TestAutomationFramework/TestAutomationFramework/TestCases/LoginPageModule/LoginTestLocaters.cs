67967280910a5c3c24f15289d551e7bf8351e5c8
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-18 10:48:42 +0500

    Inserted TestAutomationFramework/TestAutomationFramework/TestCases/LoginPageModule/LoginTestLocaters.cs at 18/05/2022 10:48:41

---
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/LoginPageModule/LoginTestLocaters.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestAutomationFramework.LocatersMethods;
+
+namespace TestAutomationFramework.TestCases.LoginPageModule
+{
+    public partial class LoginPageTestCases
+    {
+        public static void SetLoginLocaters()
+        {
+            LocatorsMethods.SetLocater("dashboardByXpath", By.XPath("//*[@id='content']/div/div[1]/h1"));
+            LocatorsMethods.SetLocater("invalidById",By.Id("spanMessage"));
+        }
+    }
+}
