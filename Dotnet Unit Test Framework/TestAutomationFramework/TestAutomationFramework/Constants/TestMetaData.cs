f709ea4304724e145b8862768d32f1c5f6ecd116
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-19 21:25:52 +0500

    Inserted TestAutomationFramework/TestAutomationFramework/Constants/TestMetaData.cs at 19/05/2022 21:25:52

---
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/Constants/TestMetaData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomationFramework.Constants
+{
+    public class LoginTestMetaData
+    {
+        /// <summary>
+        /// priority of the test case
+        /// </summary>
+        public const int Priority = 1;
+        /// <summary>
+        /// category of the test case
+        /// </summary>
+        public const string LoginCategory = "Login";
+        /// <summary>
+        /// Owner of the test case
+        /// </summary>
+        public const string Owner = "Jaffar";
+        /// <summary>
+        /// Description of the test case
+        /// </summary>
+        public const string Description = "successfull login test with postive test data and negative test data and get Welcome string from dashboard and val;idation message from login";
+    }
+}
