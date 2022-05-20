9459f75addd0dd964ce76e4e229074dc0f0daf9e
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-20 18:48:13 +0500

    Inserted BDDWithSpecFlow/BDDWithSpecFlow/Properties/AssemblyInfo.cs at 20/05/2022 18:48:13

---
+++ Behaviour Driven Development/BDDWithSpecFlow/BDDWithSpecFlow/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BDDWithSpecFlow")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BDDWithSpecFlow")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bf0acbc6-b1ce-4e0d-9aaf-cc218939d052")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
