4c56ea1b36ca2b50557ba5cff30ff026927cac2b
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-20 18:48:14 +0500

    Inserted BDDWithSpecFlow/BDDWithSpecFlow/UnitTest1.cs at 20/05/2022 18:48:13

---
+++ Behaviour Driven Development/BDDWithSpecFlow/BDDWithSpecFlow/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace BDDWithSpecFlow
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+        public UnitTest1()
+        {
+            driver = new ChromeDriver();
+        }
+        public void LoadTest()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://adactinhotelapp.com/";
+        }
+
+        [TestMethod]
+        [TestCategory("Logger Login")]
+        [DataRow("jaffarabbas", "jaffarabbas", "welcome_menu", "Welcome to Adactin Group of Hotels")]
+        //[DataRow("Jaffarabbas", "Jaffarabbas", "auth_error", "Invalid Login details or Your Password might have expired. Click here to reset your password")]
+        public void LoginCombineTC03(string username, string password, string locator, string expectedMessage)
+        {
+            LoadTest();
+            driver.FindElement(By.Id("username")).SendKeys(username);
+            driver.FindElement(By.Name("password")).SendKeys(password);
+            driver.FindElement(By.ClassName("login_button")).Click();
+            string value = driver.FindElement(By.ClassName(locator)).Text;
+            Assert.AreEqual(expectedMessage, value, "Assertion Faild");
+            driver.Close();
+        }
+    }
+}
