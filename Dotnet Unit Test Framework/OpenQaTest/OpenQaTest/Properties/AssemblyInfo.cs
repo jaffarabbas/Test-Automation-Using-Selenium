6ecf6856c96b6c6fd9abd323f48a7203bd77d961
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-21 17:32:39 +0500

    Inserted OpenQaTest/OpenQaTest/Properties/AssemblyInfo.cs at 21/05/2022 17:32:39

---
+++ Dotnet Unit Test Framework/OpenQaTest/OpenQaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OpenQaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OpenQaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6253594e-02cb-40af-99bd-cfa69e2a80ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
