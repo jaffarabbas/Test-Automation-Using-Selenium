d58b7ccc2440050a6648223b1d07a54a78128167
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-21 17:32:43 +0500

    Updated TestAutomationFramework/TestAutomationFramework/Properties/AssemblyInfo.cs at 21/05/2022 17:32:43

--- Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/Properties/AssemblyInfo.cs
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/Properties/AssemblyInfo.cs
@@ -18,3 +18,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+//logg4net 
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
