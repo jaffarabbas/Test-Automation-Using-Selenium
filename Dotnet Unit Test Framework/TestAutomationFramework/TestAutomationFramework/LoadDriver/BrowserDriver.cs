9bce6ddf10b4c6670eae15e070c5e7081e7b02b3
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-22 10:33:31 +0500

    Inserted TestAutomationFramework/TestAutomationFramework/LoadDriver/BrowserDriver.cs at 22/05/2022 10:33:31

---
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/LoadDriver/BrowserDriver.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestAutomationFramework.Constants;
+
+namespace LoadDriver
+{
+    public partial class LoadDriverInitialiazer
+    {
+        /// <summary>
+        /// Dictionary that store browser driver 
+        /// </summary>
+        private static Dictionary<string, dynamic> BrowserDriver = new Dictionary<string, dynamic>();
+
+        /// <summary>
+        /// Set browsers to the dictionary
+        /// </summary>
+        /// <param name="browserOptions"></param>
+        private static void SetBrowserDriver(dynamic browserOptions)
+        {
+            BrowserDriver.Add(Browsers.ChromeBrowser, new ChromeDriver(browserOptions));
+        }
+    }
+}
