47fde8591d51e3ff2f5f84d363c542f05722a6d6
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-26 20:35:59 +0500

    Updated TestAutomationFramework/TestAutomationFramework/TestCases/LoginPageModule/LoginTestLocaters.cs at 26/05/2022 20:35:59

--- Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/LoginPageModule/LoginTestLocaters.cs
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/LoginPageModule/LoginTestLocaters.cs
@@ -14,6 +14,10 @@ namespace TestAutomationFramework.TestCases.LoginPageModule
         {
             LocatorsMethods.SetLocater("dashboardByXpath", By.XPath("//*[@id='content']/div/div[1]/h1"));
             LocatorsMethods.SetLocater("invalidById",By.Id("spanMessage"));
+            //logout locaters
+            LocatorsMethods.SetLocater("welcomeDropdownById", By.Id("welcome"));
+            LocatorsMethods.SetLocater("logoutBtnXpath",By.XPath("//*[@id='welcome-menu']/ul/li[3]/a"));
+            LocatorsMethods.SetLocater("loginPanelTextById",By.Id("logInPanelHeading"));
         }
     }
 }
