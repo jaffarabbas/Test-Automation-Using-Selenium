c5a693a26d63181c9176633fee049c8aa245ff19
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-26 20:35:57 +0500

    Updated TestAutomationFramework/TestAutomationFramework/Constants/TestMetaData.cs at 26/05/2022 20:35:57

--- Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/Constants/TestMetaData.cs
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/Constants/TestMetaData.cs
@@ -8,21 +8,43 @@ namespace TestAutomationFramework.Constants
 {
     public class LoginTestMetaData
     {
-        /// <summary>
-        /// priority of the test case
-        /// </summary>
-        public const int Priority = 1;
-        /// <summary>
-        /// category of the test case
-        /// </summary>
-        public const string LoginCategory = "Login";
-        /// <summary>
-        /// Owner of the test case
-        /// </summary>
-        public const string Owner = "Jaffar";
-        /// <summary>
-        /// Description of the test case
-        /// </summary>
-        public const string Description = "successfull login test with postive test data and negative test data and get Welcome string from dashboard and val;idation message from login";
+        public class TestCase001
+        {
+            /// <summary>
+            /// priority of the test case
+            /// </summary>
+            public const int Priority = 1;
+            /// <summary>
+            /// category of the test case
+            /// </summary>
+            public const string LoginCategory = "Login";
+            /// <summary>
+            /// Owner of the test case
+            /// </summary>
+            public const string Owner = "Jaffar";
+            /// <summary>
+            /// Description of the test case
+            /// </summary>
+            public const string Description = "successfull login test with postive test data and negative test data and get Welcome string from dashboard and val;idation message from login";
+        }
+        public class TestCase002
+        {
+            /// <summary>
+            /// priority of the test case
+            /// </summary>
+            public const int Priority = 1;
+            /// <summary>
+            /// category of the test case
+            /// </summary>
+            public const string LoginCategory = "Login";
+            /// <summary>
+            /// Owner of the test case
+            /// </summary>
+            public const string Owner = "Jaffar";
+            /// <summary>
+            /// Description of the test case
+            /// </summary>
+            public const string Description = "successfull logout test";
+        }
     }
 }
