7e7d183a6b9838fa06283069e63dbef96b7a8572
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-27 13:13:42 +0500

    Inserted TestAutomationFramework/TestAutomationFramework/Reporting/InitializeReport.cs at 27/05/2022 13:13:42

---
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/Reporting/InitializeReport.cs
@@ -0,0 +1,83 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TestAutomationFramework.Constants;
+using TestAutomationFramework.LoggerHandler;
+
+namespace TestAutomationFramework.Reporting
+{
+    public class InitializeReport
+    {
+        #region ExtentReport Methods
+
+        private ExtentReports extentReports;
+        private ExtentTest extentTest;
+
+        public void ExtentInitialize()
+        {
+            try
+            {
+                MessageBox.Show("asd");
+                var report = new ExtentHtmlReporter(@"Report.html");
+                extentReports = new ExtentReports();
+                extentReports.AttachReporter(report);
+            }
+            catch (Exception error)
+            {
+                LogHandler.LogHandlerObject().GetLogger().Error(error.ToString());
+            }
+        }
+
+        public ExtentReports GetExtentReports()
+        {
+            try
+            {
+                return extentReports;
+            }
+            catch (Exception error)
+            {
+                LogHandler.LogHandlerObject().GetLogger().Error(error.ToString());
+                return null;
+            }
+        }
+
+
+        public ExtentTest GetExtentTest()
+        {
+            try
+            {
+                return extentTest;
+            }
+            catch (Exception error)
+            {
+                LogHandler.LogHandlerObject().GetLogger().Error(error.ToString());
+                return null;
+            }
+        }
+
+        public void ExtentFlush()
+        {
+            extentReports.Flush();
+        }
+
+       
+        #endregion
+
+        #region ExtentReports Singleton Object
+
+        //private static InitializeReport initializeReport;
+
+        //public static InitializeReport InitializeReportObject()
+        //{
+        //    initializeReport = new InitializeReport();
+        //    return initializeReport;
+        //}
+
+        #endregion
+    }
+}
