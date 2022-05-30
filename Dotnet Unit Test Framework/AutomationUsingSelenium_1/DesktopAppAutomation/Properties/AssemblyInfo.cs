95d5a84f4b223aa4f16bea3dca6be61016470427
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-30 17:29:26 +0500

    Inserted AutomationUsingSelenium_1/DesktopAppAutomation/Properties/AssemblyInfo.cs at 30/05/2022 17:29:26

---
+++ Dotnet Unit Test Framework/AutomationUsingSelenium_1/DesktopAppAutomation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DesktopAppAutomation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DesktopAppAutomation")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6a93258c-8ae9-49ab-9ec9-27b5e18aecba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
