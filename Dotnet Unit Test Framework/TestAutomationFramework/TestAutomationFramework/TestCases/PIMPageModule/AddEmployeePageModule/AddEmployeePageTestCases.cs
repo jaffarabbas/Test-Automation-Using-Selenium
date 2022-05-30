ce30b30576c06c2ecbcf5a8814a3f65b4e221f05
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-30 18:11:34 +0500

    Updated TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/AddEmployeePageTestCases.cs at 30/05/2022 18:11:33

--- Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/AddEmployeePageTestCases.cs
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/AddEmployeePageTestCases.cs
@@ -27,10 +27,6 @@ namespace TestAutomationFramework.TestCases.PIMPageModule.AddEmployeePageModule
     {
         #region Instance Properties
 
-        /// <summary>
-        /// Object instances
-        /// </summary>
-        public static LoginPageMethods loginPageMethods;
 
         #endregion
 
@@ -63,34 +59,25 @@ namespace TestAutomationFramework.TestCases.PIMPageModule.AddEmployeePageModule
         [ClassInitialize]
         public static void ClassInit(TestContext context)
         {
-            InitializeReport.InitializeReportObject.ExtentInitialize();
-            LoadDriverInitialiazer.LoadWebDriver();
-            loginPageMethods = new LoginPageMethods();
-            //Initialized Class Locaters
-            SetAddEmployeeLocaters();
+            
         }
 
         [ClassCleanup]
         public static void ClassCleanup()
         {
-            InitializeReport.InitializeReportObject.ExtentFlush();
-            GlobalInstances.ClearInstancesDictionary();
-            LocatorsMethods.ClearDictionary();
+            
         }
 
         [TestInitialize]
         public void TestInit()
         {
-            GlobalInstances.SetInstancesDictionary("testData", TestContext.DataRow["testData"].ToString());
-            GlobalInstances.SetInstancesDictionary("username", TestContext.DataRow["username"].ToString());
-            GlobalInstances.SetInstancesDictionary("password", TestContext.DataRow["password"].ToString());
-            GlobalInstances.SetInstancesDictionary("message", TestContext.DataRow["message"].ToString());
+
         }
 
         [TestCleanup]
         public void TestCleanup()
         {
-            GlobalInstances.ClearInstancesDictionary();
+            
         }
 
         #endregion
@@ -98,88 +85,9 @@ namespace TestAutomationFramework.TestCases.PIMPageModule.AddEmployeePageModule
         #region Test Cases
 
         [TestMethod]
-        [TestCategory(LoginTestMetaData.TestCase001.LoginCategory)]
-        [Priority(LoginTestMetaData.TestCase001.Priority)]
-        [DataSource(ApplicationConstant.XmlConnectionString, ApplicationConstant.XmlPath, ApplicationConstant.TestCasesOfLogin, ApplicationConstant.AccessMethod)]
-        [Description(LoginTestMetaData.TestCase001.Description)]
-        [Owner(LoginTestMetaData.TestCase001.Owner)]
-        public void TestLoginFunctionality001()
+        public void Test()
         {
-            try
-            {
-                #region Initialization
 
-                InitializeReport.InitializeReportObject.CreateTest(TestContext.TestName, LoginTestMetaData.TestCase001.Description);
-
-                #endregion
-
-                #region Working
-
-                loginPageMethods.Login(GlobalInstances.GetInstancesDictionary());
-
-                #endregion
-
-                #region Checking Assertion
-
-                string actualMessage = loginPageMethods.CheckAssertionMessage(GlobalInstances.GetInstancesDictionary(), LoginTestMetaData.TestCase001.LoginCategory);
-                Assert.AreEqual(actualMessage, GlobalInstances.GetInstancesDictionary()["message"], Errors.AssertionFailed);
-
-                #endregion
-
-                #region Reporting 
-
-                InitializeReport.InitializeReportObject.CreateLog(Status.Pass, ExtentLogger.Passed);
-
-                #endregion
-            }
-            catch (Exception error)
-            {
-                InitializeReport.InitializeReportObject.CreateLog(Status.Error, ExtentLogger.Error + error.ToString());
-            }
-        }
-
-
-        [TestMethod]
-        [TestCategory(LoginTestMetaData.TestCase002.LoginCategory)]
-        [Priority(LoginTestMetaData.TestCase002.Priority)]
-        [DataSource(ApplicationConstant.XmlConnectionString, ApplicationConstant.XmlPath, ApplicationConstant.TestCasesOfLogout, ApplicationConstant.AccessMethod)]
-        [Description(LoginTestMetaData.TestCase002.Description)]
-        [Owner(LoginTestMetaData.TestCase002.Owner)]
-        public void TestLogoutFunctionality002()
-        {
-            try
-            {
-                #region Initialization
-
-                InitializeReport.InitializeReportObject.CreateTest(TestContext.TestName, LoginTestMetaData.TestCase002.Description);
-
-                #endregion
-
-                #region Working
-
-                //loginPageMethods.Login(GlobalInstances.GetInstancesDictionary());
-
-                loginPageMethods.LogOut();
-
-                #endregion
-
-                #region Checking Assertion
-
-                string actualMessage = loginPageMethods.CheckAssertionMessage(GlobalInstances.GetInstancesDictionary(), LoginTestMetaData.TestCase002.LoginCategory);
-                Assert.AreEqual(actualMessage, GlobalInstances.GetInstancesDictionary()["message"], Errors.AssertionFailed);
-
-                #endregion
-
-                #region Reporting 
-
-                InitializeReport.InitializeReportObject.CreateLog(Status.Pass, ExtentLogger.Passed);
-
-                #endregion
-            }
-            catch (Exception error)
-            {
-                InitializeReport.InitializeReportObject.CreateLog(Status.Error, ExtentLogger.Error + error.ToString());
-            }
         }
 
         #endregion
