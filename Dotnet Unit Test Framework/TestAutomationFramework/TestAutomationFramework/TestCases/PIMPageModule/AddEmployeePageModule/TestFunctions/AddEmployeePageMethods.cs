4ab6f26c96596c0f7c93aad2c64bb438956bc15a
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-30 18:11:34 +0500

    Updated TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/TestFunctions/AddEmployeePageMethods.cs at 30/05/2022 18:11:34

--- Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/TestFunctions/AddEmployeePageMethods.cs
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/TestFunctions/AddEmployeePageMethods.cs
@@ -19,67 +19,7 @@ namespace TestAutomationFramework.TestCases.PIMPageModule.AddEmployeePageModule.
 {
     public class AddEmployeePageMethods : LoadDriverInitialiazer
     {
-        public void Login(Dictionary<string,string> attributes)
-        {
-            try
-            {
-                LocatorsMethods.RefreshPage();
-                var loginLocators = LocatorsMethods.SetByLoacator("input", "id");
-                LocatorsMethods.SetSendKeys(loginLocators["txtUsername"], attributes["username"]);
-                InitializeReport.InitializeReportObject.CreateInfo(ExtentActions.EnterInTextFeild(attributes["username"]));
-                LocatorsMethods.SetSendKeys(loginLocators["txtPassword"], attributes["password"]);
-                InitializeReport.InitializeReportObject.CreateInfo(ExtentActions.EnterInTextFeild(attributes["password"]));
-                LocatorsMethods.SetClick(loginLocators["btnLogin"]);
-                InitializeReport.InitializeReportObject.CreateInfo(ExtentActions.CLickedButton(loginLocators["btnLogin"].ToString()));
-                LocatorsMethods.ClearDictionary(LocatorsMethods.SetByLoacator("input", "id"));
-                LocatorsMethods.ClearDictionary(loginLocators);
-            }
-            catch (Exception error)
-            {
-                InitializeReport.InitializeReportObject.CreateLog(Status.Error, ExtentLogger.Error + error.ToString());
-            }
-        }
-
-        public void LogOut()
-        {
-            LocatorsMethods.SetClick(LocatorsMethods.GetLocater()["welcomeDropdownById"]);
-            InitializeReport.InitializeReportObject.CreateInfo(ExtentActions.CLickedButton(LocatorsMethods.GetLocater()["welcomeDropdownById"].ToString()));
-            Thread.Sleep(2000);
-            LocatorsMethods.SetClick(LocatorsMethods.GetLocater()["logoutBtnXpath"]);
-            InitializeReport.InitializeReportObject.CreateInfo(ExtentActions.CLickedButton(LocatorsMethods.GetLocater()["logoutBtnXpath"].ToString()));
-        }
-
-        public string CheckAssertionMessage(Dictionary<string, string> attributes,string testcase)
-        {
-            #region Login assertion
-
-            if (testcase == LoginTestMetaData.TestCase001.LoginCategory)
-            {
-                if (Validation.CheckTestData(attributes["testData"]))
-                {
-                    return LocatorsMethods.GetText(LocatorsMethods.GetLocater()["dashboardByXpath"]);
-                }
-                else
-                {
-                    return LocatorsMethods.GetText(LocatorsMethods.GetLocater()["invalidById"]);
-                }
-            }
-
-
-            #endregion
-
-            #region Logout asserion
-
-            if (testcase == LoginTestMetaData.TestCase002.LoginCategory)
-            {
-                return LocatorsMethods.GetText(LocatorsMethods.GetLocater()["loginPanelTextById"]);
-            }
-
-            #endregion
-
-            return null;
-        }
-
+       
         #region ExtentReports Singleton Object
 
         private static AddEmployeePageMethods addEmployeePageMethodsObjects;
