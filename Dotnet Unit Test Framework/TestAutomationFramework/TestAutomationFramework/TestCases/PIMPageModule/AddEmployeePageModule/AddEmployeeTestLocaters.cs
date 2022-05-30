1afebe94ef22905dd8d41de99a2573d30396cd86
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-30 18:11:34 +0500

    Updated TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/AddEmployeeTestLocaters.cs at 30/05/2022 18:11:34

--- Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/AddEmployeeTestLocaters.cs
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/AddEmployeeTestLocaters.cs
@@ -12,12 +12,7 @@ namespace TestAutomationFramework.TestCases.PIMPageModule.AddEmployeePageModule
     {
         private static void SetAddEmployeeLocaters()
         {
-            LocatorsMethods.SetLocater("dashboardByXpath", By.XPath("//*[@id='content']/div/div[1]/h1"));
-            LocatorsMethods.SetLocater("invalidById",By.Id("spanMessage"));
-            //logout locaters
-            LocatorsMethods.SetLocater("welcomeDropdownById", By.Id("welcome"));
-            LocatorsMethods.SetLocater("logoutBtnXpath",By.XPath("//*[@id='welcome-menu']/ul/li[3]/a"));
-            LocatorsMethods.SetLocater("loginPanelTextById",By.Id("logInPanelHeading"));
+           
         }
     }
 }
