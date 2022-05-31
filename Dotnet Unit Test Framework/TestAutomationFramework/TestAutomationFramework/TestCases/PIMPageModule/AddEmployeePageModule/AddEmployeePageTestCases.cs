7d27d9e0507d1cb0af2dc7006fe1f740434bbea2
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-31 23:10:03 +0500

    Updated TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/AddEmployeePageTestCases.cs at 31/05/2022 23:10:03

--- Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/AddEmployeePageTestCases.cs
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/AddEmployeePageTestCases.cs
@@ -9,12 +9,14 @@ using LoadDriver;
 using TestAutomationFramework.Constants;
 using System.Windows.Forms;
 using System.Data;
+using System.Threading;
 using TestAutomationFramework.LocatersMethods;
 using TestAutomationFramework.GlobalItems;
 using TestAutomationFramework.TestCases.LoginPageModule;
 using TestAutomationFramework.LoggerHandler;
 using TestAutomationFramework.Reporting;
 using AventStack.ExtentReports;
+using TestAutomationFramework.TestCases.PIMPageModule.AddEmployeePageModule.TestFunctions;
 
 namespace TestAutomationFramework.TestCases.PIMPageModule.AddEmployeePageModule
 {
@@ -44,40 +46,35 @@ namespace TestAutomationFramework.TestCases.PIMPageModule.AddEmployeePageModule
 
         #region Initializations and Cleanups
 
-        [AssemblyInitialize]
-        public static void AssemblyInit(TestContext context)
-        {
-
-        }
-
-        [AssemblyCleanup]
-        public static void AssemblyCleanup()
-        {
-
-        }
-
         [ClassInitialize]
         public static void ClassInit(TestContext context)
         {
-            
+            InitializeReport.InitializeReportObject.ExtentInitialize();
+            LoadDriverInitialiazer.LoadWebDriver();
+            //Initialized Class Locaters
+            SetAddEmployeeLocaters();
         }
 
         [ClassCleanup]
         public static void ClassCleanup()
         {
-            
+            GlobalInstances.ClearInstancesDictionary();
+            LocatorsMethods.ClearDictionary();
+            InitializeReport.InitializeReportObject.ExtentFlush();
         }
 
         [TestInitialize]
         public void TestInit()
         {
-
+            GlobalInstances.SetInstancesDictionary("testData", TestContext.DataRow["testData"].ToString());
+            GlobalInstances.SetInstancesDictionary("username", TestContext.DataRow["username"].ToString());
+            GlobalInstances.SetInstancesDictionary("password", TestContext.DataRow["password"].ToString());
         }
 
         [TestCleanup]
         public void TestCleanup()
         {
-            
+            GlobalInstances.ClearInstancesDictionary();
         }
 
         #endregion
@@ -85,9 +82,44 @@ namespace TestAutomationFramework.TestCases.PIMPageModule.AddEmployeePageModule
         #region Test Cases
 
         [TestMethod]
-        public void Test()
+        [TestCategory(AddEmployeeMetaData.TestCase001.Category)]
+        [Priority(AddEmployeeMetaData.TestCase001.Priority)]
+        [DataSource(ApplicationConstant.XmlConnectionString, ApplicationConstant.XmlPath, ApplicationConstant.TestCasesOfAddEmployee, ApplicationConstant.AccessMethod)]
+        [Description(AddEmployeeMetaData.TestCase001.Description)]
+        [Owner(AddEmployeeMetaData.TestCase001.Owner)]
+        public void TestAddEmployee003()
         {
+            try
+            {
+                #region Initialization
 
+                InitializeReport.InitializeReportObject.CreateTest(TestContext.TestName, AddEmployeeMetaData.TestCase001.Description);
+
+                #endregion
+
+                #region Working
+
+                LoginPageMethods.LoginPageMethodsObject.Login(GlobalInstances.GetInstancesDictionary());
+                Thread.Sleep("5000");
+                AddEmployeePageMethods.AddEmployeePageMethodsObjects.AddEmployee();
+
+                #endregion
+
+                #region Checking Assertion
+
+
+                #endregion
+
+                #region Reporting 
+
+                InitializeReport.InitializeReportObject.CreateLog(Status.Pass, ExtentLogger.Passed);
+
+                #endregion
+            }
+            catch (Exception error)
+            {
+                InitializeReport.InitializeReportObject.CreateLog(Status.Error, ExtentLogger.Error + error.ToString());
+            }
         }
 
         #endregion
