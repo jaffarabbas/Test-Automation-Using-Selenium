a4fec1d138e87b4014b1c1aed7f8fc5a285d12b2
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-02 22:43:26 +0500

    Inserted TestAutomationFramework/TestAutomationFramework/HelperMethods/Wait.cs at 02/06/2022 22:43:26

---
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/HelperMethods/Wait.cs
@@ -0,0 +1,31 @@
+﻿using AventStack.ExtentReports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TestAutomationFramework.Constants;
+using TestAutomationFramework.Reporting;
+using TestAutomationFramework.Validations;
+
+namespace TestAutomationFramework.HelperMethod
+{
+    public partial class HelperMethods
+    {
+        public static void Wait(int miliSeconds)
+        {
+            try
+            {
+                if (Validation.CheckIntegers(miliSeconds))
+                {
+                    Thread.Sleep(miliSeconds);
+                }
+            }
+            catch(Exception error)
+            {
+                InitializeReport.InitializeReportObject.CreateLog(Status.Error, ExtentLogger.Error + error.ToString());
+            }
+        }
+    }
+}
