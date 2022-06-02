436df430b0a1122e9311706674dee2394fd01d1f
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-02 22:43:31 +0500

    Updated TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/TestFunctions/AddEmployeePageMethods.cs at 02/06/2022 22:43:31

--- Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/TestFunctions/AddEmployeePageMethods.cs
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/TestFunctions/AddEmployeePageMethods.cs
@@ -14,25 +14,62 @@ using TestAutomationFramework.Validations;
 using TestAutomationFramework.Constants;
 using TestAutomationFramework.Reporting;
 using AventStack.ExtentReports;
+using TestAutomationFramework.HelperMethod;
 
 namespace TestAutomationFramework.TestCases.PIMPageModule.AddEmployeePageModule.TestFunctions
 {
     public class AddEmployeePageMethods : LoadDriverInitialiazer
     {
        
-        public void AddEmployee()
+        public void AddEmployee(Dictionary<string, string> attributes)
         {
             try
             {
-                var addEmployeeLocators = LocatorsMethods.SetByLoacator("input", "id");
+                LocatorsMethods.RefreshPage();
                 //click navigation button
                 LocatorsMethods.SetClick(LocatorsMethods.GetLocater()["clickPIMById"]);
-                LocatorsMethods.SetClick(LocatorsMethods.GetLocater()["clicAddEmployeeById"]);
+                LocatorsMethods.SetClick(LocatorsMethods.GetLocater()["clickAddEmployeeById"]);
+                HelperMethods.Wait(WaitTime.standerdWait);
+                var addEmployeeLocators = LocatorsMethods.SetByLoacator("input", "id");
+                HelperMethods.SetDictionaryScrapperLog(addEmployeeLocators);
+
+                #region Fill Add Employee Form
+
+                LocatorsMethods.SetSendKeys(addEmployeeLocators["firstName"], attributes["firstname"]);
+                LocatorsMethods.SetSendKeys(addEmployeeLocators["middleName"], attributes["middlename"]);
+                LocatorsMethods.SetSendKeys(addEmployeeLocators["lastName"], attributes["lastname"]);
+                LocatorsMethods.SetSendKeys(addEmployeeLocators["photofile"], attributes["profile"]);
+                LocatorsMethods.SetSendKeys(addEmployeeLocators["photofile"], attributes["profile"]);
+                LocatorsMethods.SetClick(addEmployeeLocators["chkLogin"]);
+                LocatorsMethods.SetSendKeys(addEmployeeLocators["user_name"], attributes["newUsername"]);
+                LocatorsMethods.SetSendKeys(addEmployeeLocators["user_password"], attributes["userPassword"]);
+                LocatorsMethods.SetSendKeys(addEmployeeLocators["re_password"], attributes["confirmPassowrd"]);
+                LocatorsMethods.SetSendKeys(LocatorsMethods.GetLocater()["setDropdownStatusById"], attributes["statusDropdown"]);
+                LocatorsMethods.SetClick(addEmployeeLocators["btnSave"]);
+               
+                #endregion
             }
             catch (Exception error)
             {
                 InitializeReport.InitializeReportObject.CreateLog(Status.Error, ExtentLogger.Error + error.ToString());
             }
+        }
+
+        public string CheckAssertionMessage(Dictionary<string, string> attributes, string testcase)
+        {
+            if (testcase == AddEmployeeMetaData.TestCase001.Category)
+            {
+                if (Validation.CheckTestData(attributes["testData"]))
+                {
+                    return LocatorsMethods.GetText(LocatorsMethods.GetLocater()["getTextAfterAddEmployee"]);
+
+                }
+                else
+                {
+                    return LocatorsMethods.GetText(LocatorsMethods.GetLocater()["getTextAfterInvalidTest"]);
+                }
+            }
+            return null;
         }
 
         #region ExtentReports Singleton Object
