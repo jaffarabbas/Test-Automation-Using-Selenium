48c97e24bfda38abb0d86629fcbf28bba63b9dba
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-03 14:29:38 +0500

    Inserted Selenium_Automation/DataRowExicution/UnitTest1.cs at 03/06/2022 14:29:38

---
+++ Dotnet Unit Test Framework/Selenium_Automation/DataRowExicution/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace AutomationUsingSelenium_1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+        public UnitTest1()
+        {
+            driver = new ChromeDriver();
+        }
+        public void LoadTest()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://adactinhotelapp.com/";
+        }
+
+        [TestMethod]
+        [TestCategory("Login")]
+        [DataRow("Jaffarabbas","Jaffarabbas", "auth_error", "Invalid Login details or Your Password might have expired. Click here to reset your password")]
+        public void LoginWithInvalidUserNameAndPassowordTC01(string username,string password,string locator,string expectedMessage)
+        {
+            LoadTest();
+            driver.FindElement(By.Id("username")).SendKeys(username);
+            driver.FindElement(By.Name("password")).SendKeys(password);
+            driver.FindElement(By.ClassName("login_button")).Click();
+            string value = driver.FindElement(By.ClassName(locator)).Text;
+            Assert.AreEqual(expectedMessage, value, "Assertion Faild");
+            driver.Close();
+        }
+
+        [TestMethod]
+        [TestCategory("Login")]
+        [DataRow("jaffarabbas", "jaffarabbas", "welcome_menu", "Welcome to Adactin Group of Hotels")]
+        public void LoginWithValidUserNameAndPassowordTC02(string username, string password, string locator, string expectedMessage)
+        {
+            LoadTest();
+            driver.FindElement(By.Id("username")).SendKeys(username);
+            driver.FindElement(By.Name("password")).SendKeys(password);
+            driver.FindElement(By.ClassName("login_button")).Click();
+            string value = driver.FindElement(By.ClassName(locator)).Text;
+            Assert.AreEqual(expectedMessage, value, "Assertion Faild");
+            driver.Close();
+        }
+
+        [TestMethod]
+        [TestCategory("Login")]
+        [DataRow("jaffarabbas", "jaffarabbas", "welcome_menu", "Welcome to Adactin Group of Hotels")]
+        [DataRow("Jaffarabbas", "Jaffarabbas", "auth_error", "Invalid Login details or Your Password might have expired. Click here to reset your password")]
+        public void LoginCombineTC03(string username, string password, string locator, string expectedMessage)
+        {
+            LoadTest();
+            driver.FindElement(By.Id("username")).SendKeys(username);
+            driver.FindElement(By.Name("password")).SendKeys(password);
+            driver.FindElement(By.ClassName("login_button")).Click();
+            string value = driver.FindElement(By.ClassName(locator)).Text;
+            Assert.AreEqual(expectedMessage, value, "Assertion Faild");
+            driver.Close();
+        }
+    }
+}
