d17be99afc73a3a4e5072c693c5ba51a7c09a221
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-03 14:29:45 +0500

    Inserted Selenium_Automation/MultipleBrowserExicution/LoadDriver.cs at 03/06/2022 14:29:45

---
+++ Dotnet Unit Test Framework/Selenium_Automation/MultipleBrowserExicution/LoadDriver.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleBrowserExicution
+{
+    public class LoadDriver
+    {
+        public IWebDriver driver;
+        //private string ExecutionBrowser = ConfigurationManager.AppSettings.Get("ExecutionBrowser");
+
+        public void InitializeDriver(string browser)
+        {
+            if(browser == "Chrome")
+            {
+                driver = new ChromeDriver();
+            }
+            else if (browser == "FireFox")
+            {
+                //FirefoxOptions options = new FirefoxOptions();
+                //options.AddArguments("--headless");
+                driver = new FirefoxDriver();
+            }
+        }
+    }
+}
