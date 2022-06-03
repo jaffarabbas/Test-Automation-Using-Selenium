f00f7f67c3932c7e2253d831a08d1020a51e4bf1
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-03 14:29:42 +0500

    Inserted Selenium_Automation/HeadLessExecution/Properties/AssemblyInfo.cs at 03/06/2022 14:29:42

---
+++ Dotnet Unit Test Framework/Selenium_Automation/HeadLessExecution/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HeadLessExecution")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HeadLessExecution")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f097e5f-223b-4cb4-859b-86a6a89d1c8b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
