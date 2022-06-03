90e93e37b60a3ec7116da9bc10eb1a25e5626cb3
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-03 14:29:45 +0500

    Inserted Selenium_Automation/MultipleBrowserExicution/Properties/AssemblyInfo.cs at 03/06/2022 14:29:45

---
+++ Dotnet Unit Test Framework/Selenium_Automation/MultipleBrowserExicution/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MultipleBrowserExicution")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MultipleBrowserExicution")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2049abf3-f2c6-47e7-8cef-376a3fa71b36")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
