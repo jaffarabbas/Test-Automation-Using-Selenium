708ab3154acef63e435c38353c33714937b1709b
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-03 14:29:46 +0500

    Inserted Selenium_Automation/PageObjectModel/LoginPage.cs at 03/06/2022 14:29:46

---
+++ Dotnet Unit Test Framework/Selenium_Automation/PageObjectModel/LoginPage.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjectModel
+{
+    public class LoginPage : LoadDriver
+    {
+        public void LoginWithInvalidUserNameAndPassoword(string username, string password, string locator, string expectedMessage)
+        {
+            LoadTest();
+            driver.FindElement(By.Id("username")).SendKeys(username);
+            driver.FindElement(By.Name("password")).SendKeys(password);
+            driver.FindElement(By.ClassName("login_button")).Click();
+            string value = driver.FindElement(By.ClassName(locator)).Text;
+            Assert.AreEqual(expectedMessage, value, "Assertion Faild");
+            driver.Close();
+        }
+    }
+}
