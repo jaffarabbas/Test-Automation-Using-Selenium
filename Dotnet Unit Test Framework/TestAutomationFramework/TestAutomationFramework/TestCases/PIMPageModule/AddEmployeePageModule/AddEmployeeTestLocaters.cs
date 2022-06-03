5c4f91851c4f51be9b2a43b32dea41302605dacb
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-03 10:52:55 +0500

    Updated TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/AddEmployeeTestLocaters.cs at 03/06/2022 10:52:54

--- Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/AddEmployeeTestLocaters.cs
+++ Dotnet Unit Test Framework/TestAutomationFramework/TestAutomationFramework/TestCases/PIMPageModule/AddEmployeePageModule/AddEmployeeTestLocaters.cs
@@ -15,7 +15,7 @@ namespace TestAutomationFramework.TestCases.PIMPageModule.AddEmployeePageModule
             LocatorsMethods.SetLocater("clickPIMById", By.XPath("//*[@id='menu_pim_viewPimModule']"));
             LocatorsMethods.SetLocater("clickAddEmployeeById", By.Id("menu_pim_addEmployee"));
             LocatorsMethods.SetLocater("setDropdownStatusById", By.Id("status"));
-            LocatorsMethods.SetLocater("getTextAfterAddEmployee", By.XPath("//*[@id='pdMainContainer']/div[1]/h1"));
+            LocatorsMethods.SetLocater("getTextAfterAddEmployee", By.XPath("/html/body/div[1]/div[3]/div/div[2]/div[1]/h1"));
             LocatorsMethods.SetLocater("getTextAfterInvalidTest",By.XPath("//*[@id='content']/div/div[1]/h1"));
         }
     }
