4851b0ab7fec9ba628bd3629b72c88af185a1cf7
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-03 14:29:48 +0500

    Inserted Selenium_Automation/ParellelTestExicution/UnitTest1.cs at 03/06/2022 14:29:48

---
+++ Dotnet Unit Test Framework/Selenium_Automation/ParellelTestExicution/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+
+namespace ParellelTestExicution
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("Parellel Test")]
+        public void ParellelExicutionTC1()
+        {
+            Thread.Sleep(5000);
+        }
+
+        [TestMethod]
+        [TestCategory("Parellel Test")]
+        [DoNotParallelize]
+        public void ParellelExicutionTC2()
+        {
+            Thread.Sleep(5000);
+        }
+
+        [TestMethod]
+        [TestCategory("Parellel Test")]
+        public void ParellelExicutionTC3()
+        {
+            Thread.Sleep(5000);
+        }
+    }
+
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        [TestCategory("Parellel Test")]
+        public void ParellelExicutionTC1()
+        {
+            Thread.Sleep(5000);
+        }
+
+        [TestMethod]
+        [TestCategory("Parellel Test")]
+        [DoNotParallelize]
+        public void ParellelExicutionTC2()
+        {
+            Thread.Sleep(5000);
+        }
+
+        [TestMethod]
+        [TestCategory("Parellel Test")]
+        public void ParellelExicutionTC3()
+        {
+            Thread.Sleep(5000);
+        }
+    }
+}
