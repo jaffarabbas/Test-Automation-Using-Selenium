e1fd81955db58e49004a67ee8691e806b473d962
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-03 14:29:40 +0500

    Inserted Selenium_Automation/DropDownTesting/UnitTest1.cs at 03/06/2022 14:29:40

---
+++ Dotnet Unit Test Framework/Selenium_Automation/DropDownTesting/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace DropDownTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+        public UnitTest1()
+        {
+            driver = new ChromeDriver();
+        }
+        public void LoadTest()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://demoqa.com/select-menu";
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            LoadTest();
+            var element = driver.FindElement(By.Id("oldSelectMenu"));
+            var selectDropDown = new SelectElement(element);
+
+            selectDropDown.SelectByText("Green");
+            selectDropDown.SelectByIndex(0);
+            selectDropDown.SelectByValue("1");
+        }
+    }
+}
