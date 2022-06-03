9749bcca02d0244aef4eb1148396dc2bada5d89a
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-03 14:29:44 +0500

    Inserted Selenium_Automation/MultipleBrowser/UnitTest1.cs at 03/06/2022 14:29:44

---
+++ Dotnet Unit Test Framework/Selenium_Automation/MultipleBrowser/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace MultipleBrowser
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+        public UnitTest1()
+        {
+            driver = new ChromeDriver();
+        }
+        public void LoadTest()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://demoqa.com/browser-windows";
+        }
+
+        [TestMethod]
+        public void TestMultipleBrowser01()
+        {
+            LoadTest();
+            driver.FindElement(By.Id("windowButton")).Click();
+            var handler = driver.WindowHandles;
+            driver.SwitchTo().Window(handler[1]);
+            string valueOfChildWindow = driver.FindElement(By.Id("sampleHeading")).Text;
+            driver.SwitchTo().Window(handler[0]);
+            var valueOfParent = driver.FindElement(By.ClassName("main-header")).Text;
+            IJavaScriptExecutor js = driver as IJavaScriptExecutor;
+            js.ExecuteScript("document.getElementById('tabButton').innerHTML = '" + valueOfChildWindow + "';");
+        }
+    }
+}
