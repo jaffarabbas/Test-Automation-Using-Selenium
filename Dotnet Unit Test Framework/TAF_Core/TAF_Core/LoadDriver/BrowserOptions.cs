e4193925a3fbbb4e9be62448f579db4a226f1cdc
jaffarabbas <gamaportal8@gmail.com>
2022-11-26 22:32:05 +0500

    Updated TAF_Core/TAF_Core/LoadDriver/BrowserOptions.cs at 26/11/2022 22:32:05

--- Dotnet Unit Test Framework/TAF_Core/TAF_Core/LoadDriver/BrowserOptions.cs
+++ Dotnet Unit Test Framework/TAF_Core/TAF_Core/LoadDriver/BrowserOptions.cs
@@ -6,7 +6,6 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using TestAutomationFramework.ConfigrationMethods;
 using TestAutomationFramework.Constants;
 
