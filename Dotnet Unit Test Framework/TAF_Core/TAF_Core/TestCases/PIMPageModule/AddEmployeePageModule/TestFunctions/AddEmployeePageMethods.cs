3088cd3dde8c3a163b34b8cbbdfdc36818a36e58
jaffarabbas <gamaportal8@gmail.com>
2022-12-04 19:54:53 +0500

    Updated TAF_Core/TAF_Core/TestCases/PIMPageModule/AddEmployeePageModule/TestFunctions/AddEmployeePageMethods.cs at 04/12/2022 19:54:53

--- Dotnet Unit Test Framework/TAF_Core/TAF_Core/TestCases/PIMPageModule/AddEmployeePageModule/TestFunctions/AddEmployeePageMethods.cs
+++ Dotnet Unit Test Framework/TAF_Core/TAF_Core/TestCases/PIMPageModule/AddEmployeePageModule/TestFunctions/AddEmployeePageMethods.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using LoadDriver;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
