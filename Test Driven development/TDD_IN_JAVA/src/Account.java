d8d507fd806a1d93e2e2b34793bfe4e196e0d90f
jaffarabbas <gamaportal8@gmail.com>
2022-12-07 21:29:18 +0500

    Updated TDD_IN_JAVA/src/Account.java at 07/12/2022 21:29:18

--- Test Driven development/TDD_IN_JAVA/src/Account.java
+++ Test Driven development/TDD_IN_JAVA/src/Account.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Account {
 
     private String owner;
@@ -6,6 +11,7 @@ public class Account {
     private double CNIC;
     String fname;
     String  lname;
+    Card card;
 
     public Account(String owner, long balance, int pin, int CNIC, String fname, String lname) {
         this.owner = owner;
@@ -26,7 +32,6 @@ public class Account {
         return false;
     }
 
-
     public boolean verifyCNIC(int CNIC) {
         if (this.CNIC == CNIC) {
             return true;
@@ -40,6 +45,7 @@ public class Account {
             toAcc.balance += amount;
         }
     }
+
     public void changePin(int oldPin, int newPin){
         if (this.pin == oldPin)
             this.pin=newPin;
@@ -55,4 +61,58 @@ public class Account {
             this.balance+=amount;
     }
 
+    public void addCard(Card card){
+        this.card = card;
+        setCardValue();
+    }
+    //generalized method for assigning limits to card
+    //note
+    //card type must equal to map value
+    public void setCardValue(){
+        card.cardTypes.forEach((cardValue, limit) -> {
+            if(card.getCardType().equals(cardValue)){
+                card.setLimit(limit);
+            }
+        });
+    }
+}
+
+//create seprate instances of card
+class Card{
+    private String cardType;
+    private String cardNumber;
+    private int limit;
+
+    //dynamically get card data
+    public HashMap<String, Integer> cardTypes = new HashMap<>() {
+        {
+            put("VisaCard", 100000);
+            put("MasterCard", 50000);
+        }
+    };
+
+    public Card(String cardType, String cardNumber) {
+        this.cardType = cardType;
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public long getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public void setNewCardInCardsMap(String cardType, int limit){
+        cardTypes.put(cardType, limit);
+    }
 }
