bc25ed1f623d1527badf392e0cb894a78c76cad7
jaffarabbas <gamaportal8@gmail.com>
2024-01-18 21:48:38 +0500

    18-01-2023

---
+++ Dotnet Unit Test Framework/TestEfrotech/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestEfrotech")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestEfrotech")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a658271c-b0f5-4cb6-8866-48327a1702ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Dotnet Unit Test Framework/TestEfrotech/UnitTest1.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace TestEfrotech
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+        public UnitTest1()
+        {
+            driver = new ChromeDriver();
+        }
+        public void LoadTest()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Url = "";
+        }
+        #region Initializations and Cleanups
+
+        public TestContext instance;
+        public TestContext TestContext
+        {
+            set { instance = value; }
+            get { return instance; }
+        }
+
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+        }
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+           
+        }
+
+
+        [TestInitialize]
+        public void TestInit()
+        {
+           
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+           
+        }
+
+        #endregion
+
+        public void CLickattendance()
+        {
+            driver.FindElement(By.XPath("/html/body/form/div[3]/header/nav/div[1]/div[2]/ul/li[2]/ul/li[1]/a")).Click();
+            driver.FindElement(By.XPath("/html/body/form/div[3]/header/nav/div[1]/div[2]/ul/li[2]/ul/li[1]/ul/li[1]/a")).Click();
+        }
+
+        public void CLickLeave()
+        {
+            driver.FindElement(By.XPath("/html/body/form/div[3]/header/nav/div[1]/div[2]/ul/li[2]/ul/li[2]/a")).Click();
+            driver.FindElement(By.XPath("/html/body/form/div[3]/header/nav/div[1]/div[2]/ul/li[2]/ul/li[2]/ul/li[2]/a")).Click();
+        }
+        public void ClickDropDown(String module)
+        {
+            driver.FindElement(By.XPath("/html/body/form/div[3]/header/nav/div[1]/div[2]/ul/li[2]/a")).Click();
+            if(module == "attendance")
+            {
+                CLickattendance();
+            }else if(module == "leave")
+            {
+                CLickLeave();
+            }
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //string password = TestContext.DataRow["password"].ToString();
+            //string message = TestContext.DataRow["message"].ToString();
+            //string locator = TestContext.DataRow["locator"].ToString();
+            LoadTest();
+            driver.FindElement(By.Id("txtPin")).SendKeys("");
+            driver.FindElement(By.Id("txtUserName")).SendKeys("");
+            driver.FindElement(By.Id("txtPassword")).SendKeys("");
+            driver.FindElement(By.Id("btnLogin")).Click();
+
+            ClickDropDown("attendance");
+            ClickDropDown("leave");
+            Thread.Sleep(5000);
+            //string messageValue = driver.FindElement(By.ClassName(locator)).Text;
+            //Assert.AreEqual(message, messageValue, "Assert Failed");
+            driver.Close();
+
+        }
+    }
+}
